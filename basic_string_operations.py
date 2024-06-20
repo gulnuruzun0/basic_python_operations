e0a0e7d46a36d8e6a263760729bd92780b37e4d0
Gulnur Uzun <157987210+gulnuruzun0@users.noreply.github.com>
2024-06-20 18:58:42 +0300

    Update basic_string_operations.py

--- basic_string_operations.py
+++ basic_string_operations.py
@@ -1,9 +1,7 @@
 '''
-Gülnur Uzun, Alper Tanrıkulu
+Basic python operations
 
-Q2
-
-(30 points) 7 students took 2 exams, the top three students will get AA. You were provided with the list of exam marks, 
+7 students took 2 exams, the top three students will get AA. You were provided with the list of exam marks, 
 use Python string operations, mathematical operations and loops to find average of two exam marks for each student and choose 
 top 3 students programmatically. Announce the top 3 students to the audience:
 List = [‘Ayşe: 75-78’, ‘Berk: 80-60’, ‘Can: 58-61’, ‘Didem: 34-45’,’Erdem: 32-37’, ‘Fatih: 69-75’, ‘Gül: 54-63’]
@@ -35,4 +33,3 @@ for i in range(3):
 
     names.pop(max_index) # deleted the name having highest average grade to reach the second highest average grade
     mean_grades.pop(max_index) # deleted the average grade having highest average grade to reach the second highest average grade
-
